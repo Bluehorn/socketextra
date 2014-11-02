1490fa4498bf550152e7748b6d6d7522cf9540b1
Torsten Landschoff <torsten@landschoff.net>
2014-11-02 20:05:44 +0100

    Initial setup: created setup.py.

---
+++ DESCRIPTION.rst
@@ -0,0 +1,4 @@
+sendmsg, recvmsg etc. for Python 2.
+
+This package is not needed for Python 3 as it comes with these
+included.
---
+++ setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='socketextra',
+    version='0.0.1',
+    description='sendmsg etc. for Python2 (not needed for Py3)',
+    long_description=long_description,
+
+    url='https://github.com/Bluehorn/socketextra',
+    author='Torsten Landschoff',
+    author_email='t.landschoff@gmx.net',
+
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: System :: Networking',
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    keywords='socket sendmsg recvmsg',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+
+    install_requires=[],
+    extras_require = {},
+)
+
