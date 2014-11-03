51540d3d510372f14323710bfe825166e3b27135
Torsten Landschoff <torsten@landschoff.net>
2014-11-03 12:52:58 +0100

    Sending via iovecs/scatter gather implemented (sans freeing memory).

--- socketextra/_socketextra.c
+++ socketextra/_socketextra.c
@@ -2,6 +2,7 @@
 #include <sys/socket.h>
 
 static int extract_fd(PyObject *socket);
+static int buffers_to_iovec(PyObject *buffers, struct msghdr *msghdr);
 
 static PyObject *
 socketextra_CMSG_LEN(PyObject *self, PyObject *args)
@@ -38,16 +39,28 @@ socketextra_sendmsg(PyObject *self, PyObject *args)
     int flags = 0;
     PyObject *address = NULL;
     int sockfd = -1;
+    struct msghdr msghdr = {0};
+    ssize_t sendmsg_retval = -1;
 
     if (!PyArg_ParseTuple(args, "OO|OiO:sendmsg",
                           &socket, &buffers, &ancdata, &flags, &address))
         return NULL;
 
+    if (address && address != Py_None)
+        return PyErr_Format(PyExc_NotImplementedError, "sendmsg: the address argument is not implemented");
+
     sockfd = extract_fd(socket);
     if (sockfd == -1)
         return NULL;
 
-    return PyErr_Format(PyExc_NotImplementedError, "sendmsg");
+    if (!buffers_to_iovec(buffers, &msghdr))
+        return NULL;
+
+    sendmsg_retval = sendmsg(sockfd, &msghdr, flags);
+    if (sendmsg_retval == -1) {
+        return PyErr_SetFromErrno(PyExc_OSError);
+    }
+    return PyInt_FromSsize_t(sendmsg_retval);
 }
 
 static PyObject *
@@ -84,6 +97,45 @@ static int extract_fd(PyObject *socket)
         return -1;
     }
     return fileno_c;
+}
+
+/* Fill msghdr->msg_iov and friends from iterable buffers. Return true on success.
+   On error, return false and have PyErr set accordingly. */
+static int buffers_to_iovec(PyObject *buffers, struct msghdr *msghdr)
+{
+    Py_ssize_t buffer_count = 0, index = 0;
+    Py_buffer *py_buffers = NULL;
+    struct iovec *iovec = NULL;
+    PyObject *buffer_seq = NULL;
+
+    if (PyString_Check(buffers) || PyUnicode_Check(buffers)) {
+        // for compatibility with Python3, which does not accept a bytes instance.
+        PyErr_Format(PyExc_TypeError, "sendmsg: buffers must not be a string/unicode object");
+        return 0;
+    }
+    buffer_seq = PySequence_Fast(buffers, "sendmsg: buffers must be an interable of buffers");
+    if (!buffer_seq)
+        return 0;
+
+    buffer_count = PySequence_Fast_GET_SIZE(buffer_seq);
+    msghdr->msg_iovlen = buffer_count;
+    msghdr->msg_iov = PyMem_New(struct iovec, buffer_count);
+    py_buffers = PyMem_New(Py_buffer, buffer_count);
+    if (!msghdr->msg_iov || !py_buffers)
+        return 0;
+
+    iovec = msghdr->msg_iov;
+    for (index = 0; index < buffer_count; ++index) {
+        if (!PyArg_Parse(PySequence_Fast_GET_ITEM(buffer_seq, index),
+                "s*;sendmsg() argument 1 must be an iterable of buffers",
+                &py_buffers[index]))
+            return 0;
+        iovec->iov_base = py_buffers[index].buf;
+        iovec->iov_len = py_buffers[index].len;
+        iovec += 1;
+    }
+
+    return 1;
 }
 
 static PyMethodDef socketextra_methods[] = {
--- tests/test_socketextra.py
+++ tests/test_socketextra.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 
 import pytest
+import socket
 import sysconfig
 import socketextra
 
@@ -12,3 +13,28 @@ def test_invalid_fileno():
             return 1 << 8*sysconfig.get_config_var('SIZEOF_INT')
     with pytest.raises(OverflowError):
         socketextra.sendmsg(BadSocket(), [])
+
+
+def test_address_error():
+    """for now the address argument is not supported"""
+    sock, _ = socket.socketpair(socket.AF_UNIX, socket.SOCK_DGRAM)
+    with pytest.raises(NotImplementedError):
+        socketextra.sendmsg(sock, [], [], 0, ("127.0.0.1", "200"))
+
+
+def test_sendmsg_scather_gather():
+    """Passing multiple buffers to sendmsg will send them as a single message."""
+    sender, receiver = socket.socketpair(socket.AF_UNIX, socket.SOCK_DGRAM)
+    socketextra.sendmsg(sender, ["vec1", "vec2", "vec3"])
+    assert receiver.recv(400) == "vec1vec2vec3"
+
+
+def test_sendmsg_non_buffer():
+    """buffers must be a list of buffer objects, check error checking."""
+    sender, receiver = socket.socketpair(socket.AF_UNIX, socket.SOCK_DGRAM)
+    with pytest.raises(TypeError):
+        socketextra.sendmsg(sender, 42)
+    with pytest.raises(TypeError):
+        socketextra.sendmsg(sender, ["a string", 42])
+    with pytest.raises(TypeError):
+        socketextra.sendmsg(sender, "string is bad")
