fb230145e393077bd239ca56fe316acd320192b7
Torsten Landschoff <torsten@landschoff.net>
2014-11-07 18:54:24 +0100

    Bump version after uploading 0.0.2.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
 
 setup(
     name='socketextra',
-    version='0.0.2',
+    version='0.0.3',
     description='sendmsg etc. for Python2 (not needed for Py3)',
     long_description=long_description,
 
